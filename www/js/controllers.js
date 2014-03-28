e17e302bcd63a9bb828080ba89cb6b7df3c36cad
Christoffer Palm <christoffer.palm@oddhill.se>
2014-03-28 15:19:46 +0100

    Changes to BooksService

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,5 +1,19 @@
 angular.module('bookApp.controllers', [])
 
 .controller('BaseController', function($scope, BooksService) {
-	$scope.books =  BooksService.search('Computers');
+	$scope.books = [];
+	$scope.searchTerm = '';
+
+	BooksService.search('monkey').then(function(response) {
+		$scope.books.length = 0;
+		$scope.books = response.items;
+
+		console.log(response.items);
+	});
+
+	$scope.search = function() {
+		BooksService.search($scope.searchTerm).then(function(response) {
+			$scope.books = response.items;
+		});
+	}
 });
